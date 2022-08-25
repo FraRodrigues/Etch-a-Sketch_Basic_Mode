44c6b3d5cb9e46076c4e0509c6562b773fd88cb8
FraRodrigues <fraamrodrigues@gmail.com>
2022-08-25 20:51:22 +0100

    Changed code so squares stay inside the main container and added function to clean grid

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 const square = document.getElementById("main");
+const hoverPaint = document.querySelectorAll(".miniSquare");
 
 document.getElementById("gridSize").addEventListener("click", () => {
     let size = prompt("Choose grid size (number of squares by side)", 16);
     createGrid(size);
 })
 
-function makeSquare(newSqr) {
-    newSqr = document.createElement("div");
-    newSqr.style.cssText = `
-    border: 1px solid black;
-    width: auto;
-    height: auto;
-    `
-    return newSqr;
+document.getElementById("clean").addEventListener("click", cleanSketch);
+
+hoverPaint.forEach( (miniSquare) => {
+        miniSquare.addEventListener("onmouseover", (miniSquare) => {
+                miniSquare.style.backgroundColor = "black";
+            }
+        )
+    }
+)
+
+function styleSquare(sqrDiv,gridSize) {
+    sqrDiv.style.border = "1px solid black";
+    sqrDiv.style.height = (100/gridSize)+"%";
+    sqrDiv.style.width = "100%";
+    sqrDiv.style.backgroundColor = "white";
+    sqrDiv.style.boxSizing = "border-box";
+}
+
+function styleBreakLine (newDiv,gridSize){
+    newDiv.style.width = (100/gridSize)+"%";
 }
 
 function createGrid(gridSize) {
     for(let i = 0; i < gridSize; i++){
         let x = 0;
-        console.log(gridSize)
+        let breakLine = document.createElement("div");
+        styleBreakLine(breakLine,gridSize);
         while(x < gridSize){
-            const newSqr = makeSquare(newSqr);
-            square.appendChild(newSqr);
+            let newSqr = document.createElement("div");
+            styleSquare(newSqr,gridSize);
+            newSqr.classList.add("miniSquare")
+            breakLine.appendChild(newSqr);
             x++;
         }
-        const breakLine = document.createElement("div");
-        breakLine.innerHTML = "<br>";
+        square.appendChild(breakLine);
+
     }
 }
 
+function cleanSketch() {
+    document.querySelectorAll(".miniSquare").forEach( (miniSquare) => {
+        miniSquare.style.backgroundColor = "white";
+    }
+)
+}
 //document.getElementById("clean").addEventListener("click", createGrid)
