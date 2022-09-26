19e91191fed89a35bed03de79b5ed9448511c63c
FraRodrigues <fraamrodrigues@gmail.com>
2022-09-26 14:03:39 +0100

    Added drawing and cleaning functionality, and added limit to the size user can choose

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 const square = document.getElementById("main");
-const hoverPaint = document.querySelectorAll(".miniSquare");
 
 document.getElementById("gridSize").addEventListener("click", () => {
-    let size = prompt("Choose grid size (number of squares by side)", 16);
+    if (!!document.getElementsByClassName("break") === true){
+        while (square.hasChildNodes()){
+            square.removeChild(square.lastChild);
+        }
+    }
+    let size = prompt("Choose grid size (number of squares by side) (max 128)", 16);
+    if (size > 128){
+        alert("Maximum size is 128 squares!");
+    } else {
     createGrid(size);
+    }
 })
 
 document.getElementById("clean").addEventListener("click", cleanSketch);
 
-hoverPaint.forEach( (miniSquare) => {
-        miniSquare.addEventListener("onmouseover", (miniSquare) => {
-                miniSquare.style.backgroundColor = "black";
-            }
-        )
+
+function createGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++){
+        let x = 0;
+        let breakLine = document.createElement("div");
+        styleBreakLine(breakLine,gridSize);
+        breakLine.classList.add("break")
+        while(x < gridSize){
+            const newSqr = document.createElement("div");
+            styleSquare(newSqr,gridSize);
+            newSqr.classList.add("miniSquare")
+            newSqr.addEventListener("mouseover", () => hover(newSqr))
+            breakLine.appendChild(newSqr);
+            x++;
+        }
+        square.appendChild(breakLine);
+
     }
-)
+    
+}
+
+function styleBreakLine (newDiv,gridSize){
+    newDiv.style.width = (100/gridSize)+"%";
+}
 
 function styleSquare(sqrDiv,gridSize) {
     sqrDiv.style.border = "1px solid black";
@@ -24,31 +49,13 @@ function styleSquare(sqrDiv,gridSize) {
     sqrDiv.style.boxSizing = "border-box";
 }
 
-function styleBreakLine (newDiv,gridSize){
-    newDiv.style.width = (100/gridSize)+"%";
-}
-
-function createGrid(gridSize) {
-    for(let i = 0; i < gridSize; i++){
-        let x = 0;
-        let breakLine = document.createElement("div");
-        styleBreakLine(breakLine,gridSize);
-        while(x < gridSize){
-            let newSqr = document.createElement("div");
-            styleSquare(newSqr,gridSize);
-            newSqr.classList.add("miniSquare")
-            breakLine.appendChild(newSqr);
-            x++;
-        }
-        square.appendChild(breakLine);
-
-    }
-}
-
 function cleanSketch() {
     document.querySelectorAll(".miniSquare").forEach( (miniSquare) => {
         miniSquare.style.backgroundColor = "white";
     }
 )
 }
-//document.getElementById("clean").addEventListener("click", createGrid)
+
+function hover(square){
+    square.style.backgroundColor= "black";
+}
